2290424f7c17e55cb6d3dff4400b53b21a462de2
sivag.usa@gmail.com <sivag.usa@gmail.com>
2023-08-21 17:40:29 +0530

    updated version

---
+++ src/main/java/org/web/WebDataExtractorMain.java
@@ -0,0 +1,11 @@
+package org.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDataExtractorMain {
+    public static void main(String[] args) {
+        SpringApplication.run(WebDataExtractorMain.class, args);
+    }
+}
